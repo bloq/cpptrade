6d9f538ec2b5a1b208c6e1592e88dccf41b90769
Jeff Garzik <jeff@bloq.com>
2017-07-29 21:10:45 -0400

    Delete dead Util code.  Shuffle obsrv util code to Util.

--- obsrv.cc
+++ obsrv.cc
@@ -14,6 +14,7 @@
 #include <ctype.h>
 #include <assert.h>
 #include "Market.h"
+#include "Util.h"
 #include "srvapi.h"
 #include "srv.h"
 
@@ -61,29 +62,6 @@ get_content_length (const evhtp_request_t *req)
     return strtoll (content_len_str, NULL, 10);
 }
 
-static std::string addressToStr(const struct sockaddr *sockaddr,
-				socklen_t socklen)
-{
-        char name[1025] = "";
-        if (!getnameinfo(sockaddr, socklen, name, sizeof(name), NULL, 0, NI_NUMERICHOST))
-            return std::string(name);
-
-	return string("");
-}
-
-static std::string formatTime(const std::string& fmt, time_t t)
-{
-	// convert unix seconds-since-epoch to split-out struct
-	struct tm tm;
-	gmtime_r(&t, &tm);
-
-	// build formatted time string
-	char timeStr[fmt.size() + 256];
-	strftime(timeStr, sizeof(timeStr), fmt.c_str(), &tm);
-
-	return string(timeStr);
-}
-
 static std::string httpDateHdr(time_t t)
 {
 	return formatTime("%a, %d %b %Y %H:%M:%S GMT", t);
@@ -92,40 +70,6 @@ static std::string httpDateHdr(time_t t)
 static std::string isoTimeStr(time_t t)
 {
 	return formatTime("%FT%TZ", t);
-}
-
-static bool readJsonFile(const std::string& filename, UniValue& jval)
-{
-	jval.clear();
-
-        FILE *f = fopen(filename.c_str(), "r");
-	if (!f)
-		return false;
-
-        string jdata;
-
-        char buf[4096];
-        while (!feof(f)) {
-                int bread = fread(buf, 1, sizeof(buf), f);
-		if (ferror(f)) {
-			fclose(f);
-			return false;
-		}
-
-                string s(buf, bread);
-                jdata += s;
-        }
-
-	if (ferror(f)) {
-		fclose(f);
-		return false;
-	}
-        fclose(f);
-
-	if (!jval.read(jdata))
-		return false;
-
-        return true;
 }
 
 static void
