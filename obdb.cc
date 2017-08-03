63344f31d0ff66cad6746cf40d3827b2374a31ec
Jeff Garzik <jeff@bloq.com>
2017-08-03 00:10:31 -0400

    create db manip helper commands

--- obdb.cc
+++ obdb.cc
@@ -39,6 +39,18 @@ static struct argp_option options[] = {
 	{ "load-accounts", 1001, "FILE", 0,
 	  "JSON input file containing account data" },
 
+	{ "load-auth", 1002, "FILE", 0,
+	  "JSON input file containing account data" },
+
+	{ "keys", 1003, NULL, 0,
+	  "Dump all keys" },
+
+	{ "dump", 1004, NULL, 0,
+	  "Dump all keys and values" },
+
+	{ "clear", 1005, NULL, 0,
+	  "Delete all data, before loading" },
+
 	{ }
 };
 
@@ -47,6 +59,10 @@ static const struct argp argp = { options, parse_opt, NULL, doc };
 
 static string opt_output_fn = DEFAULT_DATASTORE_FN;
 static string load_accounts_fn;
+static string load_auth_fn;
+static bool opt_dump_keys = false;
+static bool opt_dump_db = false;
+static bool opt_clear_db = false;
 
 static error_t parse_opt (int key, char *arg, struct argp_state *state)
 {
@@ -56,8 +72,24 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state)
 		opt_output_fn = arg;
 		break;
 
-	case 1001:
+	case 1001:	// --load-acounts=file
 		load_accounts_fn = arg;
+		break;
+
+	case 1002:	// --load-auth=file
+		load_auth_fn = arg;
+		break;
+
+	case 1003:	// --keys
+		opt_dump_keys = true;
+		break;
+
+	case 1004:	// --dump
+		opt_dump_db = true;
+		break;
+
+	case 1005:
+		opt_clear_db = true;
 		break;
 
 	case ARGP_KEY_END:
@@ -79,19 +111,44 @@ static void dbPutJson(rocksdb::DB* db, const std::string& key,
 	assert(s.ok());
 }
 
-static void do_load_accounts(rocksdb::DB* db)
+static void dbPrefixedLoad(rocksdb::DB* db,
+			  const std::string& fn,
+			  const std::string& prefix)
 {
-	UniValue accounts;
-	bool rc = readJsonFile(load_accounts_fn, accounts);
-	assert(rc);
-
-	assert(accounts.getType() == UniValue::VOBJ);
-
-	const std::vector<std::string>& keys = accounts.getKeys();
-	const std::vector<UniValue>& values = accounts.getValues();
-	for (size_t i = 0; i < keys.size(); i++) {
-		dbPutJson(db, "/acct/" + keys[i], values[i]);
+	UniValue jobj;
+	bool rc = readJsonFile(fn, jobj);
+	if (!rc) {
+		perror(fn.c_str());
+		exit(1);
 	}
+
+	assert(jobj.getType() == UniValue::VOBJ);
+
+	const std::vector<std::string>& keys = jobj.getKeys();
+	const std::vector<UniValue>& values = jobj.getValues();
+	for (size_t i = 0; i < keys.size(); i++) {
+		dbPutJson(db, prefix + keys[i], values[i]);
+	}
+}
+
+static void dump_db(rocksdb::DB* db)
+{
+	rocksdb::Iterator* it = db->NewIterator(rocksdb::ReadOptions());
+	for (it->SeekToFirst(); it->Valid(); it->Next()) {
+		cout << it->key().ToString() << ": " << it->value().ToString() << endl;
+	}
+	assert(it->status().ok()); // Check for any errors found during the scan
+	delete it;
+}
+
+static void dump_keys(rocksdb::DB* db)
+{
+	rocksdb::Iterator* it = db->NewIterator(rocksdb::ReadOptions());
+	for (it->SeekToFirst(); it->Valid(); it->Next()) {
+		cout << it->key().ToString() << endl;
+	}
+	assert(it->status().ok()); // Check for any errors found during the scan
+	delete it;
 }
 
 int main(int argc, char ** argv)
@@ -104,6 +161,9 @@ int main(int argc, char ** argv)
 		return EXIT_FAILURE;
 	}
 
+	if (opt_clear_db)
+		DestroyDB(opt_output_fn, rocksdb::Options());
+
 	rocksdb::DB* db;
 	rocksdb::Options options;
 	options.create_if_missing = true;
@@ -111,8 +171,19 @@ int main(int argc, char ** argv)
 	  rocksdb::DB::Open(options, opt_output_fn, &db);
 	assert(status.ok());
 
+	// input
+
 	if (!load_accounts_fn.empty())
-		do_load_accounts(db);
+		dbPrefixedLoad(db, load_accounts_fn, "/acct/");
+	if (!load_auth_fn.empty())
+		dbPrefixedLoad(db, load_auth_fn, "/auth/");
+
+	// output
+
+	if (opt_dump_keys)
+		dump_keys(db);
+	if (opt_dump_db)
+		dump_db(db);
 
 	return 0;
 }
